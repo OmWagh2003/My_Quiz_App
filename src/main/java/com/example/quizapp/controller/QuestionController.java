3444a9a30a1299cd5181eac12fbe92a5b9321768
Om Wagh <omwagh610@gmail.com>
2024-12-12 01:53:14 +0530

    Set up "quiz app" Spring project with database integration
    - Created a database and added questions to the table.
    - Structured the project with separate packages for controller, service, model, and repository.
    - Implemented a GET endpoint to fetch all questions from the database.

---
+++ src/main/java/com/example/quizapp/controller/QuestionController.java
@@ -0,0 +1,21 @@
+package com.example.quizapp.controller;
+
+import com.example.quizapp.model.Question;
+import com.example.quizapp.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/question")
+public class QuestionController {
+    @Autowired
+    private QuestionService questionService;
+    @RequestMapping("allQuestions")
+    public List<Question> getAllQuestions(){
+        return questionService.getAllQuestions();
+
+    }
+}
---
+++ src/main/java/com/example/quizapp/service/QuestionService.java
@@ -0,0 +1,16 @@
+package com.example.quizapp.service;
+
+import com.example.quizapp.dao.QuestionDao;
+import com.example.quizapp.model.Question;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class QuestionService {
+    @Autowired
+    private QuestionDao questionDao;
+    public List<Question> getAllQuestions() {
+        return questionDao.findAll();
+    }
+}
