eaf99f50057242e46f76cf642192637984b01a12
Om Wagh <omwagh610@gmail.com>
2024-12-18 01:03:01 +0530

    Add quiz generation functionality with random questions
    - Implemented logic to fetch random question by IDs from the database.
    - Created a feature to generate a quiz with 5 questions based on category.
    - Enhanced the "quiz app" to support dynamic quiz creation.

--- src/main/java/com/example/quizapp/dao/QuestionDao.java
+++ src/main/java/com/example/quizapp/dao/QuestionDao.java
@@ -2,6 +2,8 @@ package com.example.quizapp.dao;
 
 import com.example.quizapp.model.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +12,8 @@ import java.util.List;
 public interface QuestionDao extends JpaRepository<Question, Integer> {
 
     List<Question> findByCategory(String category);
+
+    @Query(value = "SELECT * FROM question q WHERE q.category = ?1 ORDER BY RAND() LIMIT ?2", nativeQuery = true)
+    List<Question> findRandomQuestionByCategory(String category, int numQ);
+
 }
